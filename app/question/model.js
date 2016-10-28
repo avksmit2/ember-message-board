8a381a65f64d4404bb6bbd7f598de84dd4ca026d
Angie Smith <avksmit2@gmail.com>
2016-10-27 20:54:40 -0700

    add navbar, header and footer to application template, add index route.

--- app/question/model.js
+++ app/question/model.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  author: DS.attr(),
+  question: DS.attr(),
+  timestamp: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
 });
