f71e179c2f415322528c069c273b1c09e497f51b
Angie Smith <avksmit2@gmail.com>
2016-10-28 10:09:08 -0700

    fix the link to the question route.

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    saveQuestion() {
+      var params = {
+        title: this.get('title') ? this.get('title') : "",
+        author: this.get('author') ? this.get('author') : "",
+        content: this.get('content') ? this.get('content') : "",
+        icon: this.get('icon') ? this.get('icon') : "https://dummyimage.com/600x400/000/fff.png&text=??",
+        timestamp: moment().valueOf()
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -1,22 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params){
-    return this.store.findRecord('question', params.question_id);
-  }
-  // actions: {
-  //   destroyQuestion(question) {
-  //     question.destroyRecord();
-  //     this.transitionTo('index');
-  //   },
-  //   updateQuestion(question, params) {
-  //     Object.keys(params).forEach(function(key) {
-  //       if(params[key]!==undefined) {
-  //         question.set(key,params[key]);
-  //       }
-  //     });
-  //     question.save();
-  //     this.transitionTo('question');
-  //   }
+  model(params) {
+      return this.store.findRecord('question', params.question_id);
   }
 });
