ceb9d6e2a8ed60bf89717b072bcb4e35da3c27a2
Angie Smith <avksmit2@gmail.com>
2016-10-30 14:32:17 -0700

    fix the update-question, moving the action from the form to the button.

--- app/routes/question.js
+++ app/routes/question.js
@@ -34,7 +34,7 @@ export default Ember.Route.extend({
     },
     destroyAnswer(answer) {
       answer.destroyRecord();
-      this.transitionTo('question', question);
+      this.transitionTo('question');
     }
   }
 });
